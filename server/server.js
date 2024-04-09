7ada09e035674fa1f1512cbeb5693503915bde11
arunnarangali <113373895+arunnarangali@users.noreply.github.com>
2024-04-09 19:57:57 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-  origin:"http://localhost:3000",
+  origin:"https://weather-dashboard-application-two.vercel.app/",
  credentials:true, 
 }))
 dotenv.config();
